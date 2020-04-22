8138c538346adad187a96a7b1729e8b29e9fac2d
Alla Cherkes <alla.cherkes@sap.com>
2020-04-22 19:02:17 +0300

    secure client - use custome headers

--- src/unleash_client_wrapper.ts
+++ src/unleash_client_wrapper.ts
@@ -1,15 +1,16 @@
 import { initialize, Unleash, Strategy } from "unleash-client";
 import { log } from "./logger";
 import { ServerArgs } from "./server_arguments";
+import { CustomHeaders } from "unleash-client/lib/unleash";
 
 async function getUnleashClientReadyPromise(extensionName: string, client: Unleash, serverArgs: ServerArgs): Promise<void> {
   return new Promise((resolve, reject) => {
     client.on("warn", log);
     client.once("error", (err: Error) => {
-      log(`FT is initialization failed for extension ${extensionName}. EndPoint: ${serverArgs.ftServerEndPoint}`);
+      log(`FT initialization failed for extension ${extensionName}. EndPoint: ${serverArgs.ftServerEndPoint}`);
       reject(err);
     });
-    client.once("ready", () => {
+    client.once("registered", () => {
       log(`FT is initialized to server for extension ${extensionName}. EndPoint: ${serverArgs.ftServerEndPoint}`);
       resolve();
     });
@@ -17,14 +18,21 @@ async function getUnleashClientReadyPromise(extensionName: string, client: Unlea
 }
 
 export async function initializeUnleashClient(extensionName: string, serverArgs: ServerArgs, featureStrategies: Strategy[]): Promise<Unleash> {
+  const authorizationHeader: CustomHeaders = {};
+  authorizationHeader["authorization"] = "client_key1";
+
+  //let unleashClient: Unleash | undefined;
   //create a new unleash client
   const unleashClient = initialize({
     appName: extensionName,
     refreshInterval: serverArgs.ftServerInterval,
     url: serverArgs.ftServerEndPoint,
-    strategies: featureStrategies
+    strategies: featureStrategies,
+    customHeaders: authorizationHeader
   });
 
-  await getUnleashClientReadyPromise(extensionName, unleashClient, serverArgs);
+  await getUnleashClientReadyPromise(extensionName, unleashClient, serverArgs).catch(e => {
+    log(e);
+  });
   return unleashClient;
 }
