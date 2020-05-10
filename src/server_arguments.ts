21a2e36957aad23ab1080cf3244b7427080b243b
Frenkel <lior.frenkel@sap.com>
2020-05-10 21:11:54 +0300

    [refactor] refactored parse-duration module to be used with dts and import instead of require

--- src/server_arguments.ts
+++ src/server_arguments.ts
@@ -1,11 +1,10 @@
 import { log } from "./logger";
 import { getEnv } from "./utils";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const parseDuration = require("parse-duration");
+import parseDuration from "parse-duration";
 
 export const ENV_FT_SERVER_ENDPOINT_NAME = "FT_SERVER_ENDPOINT";
 export const ENV_REFRESH_INTERVAL_NAME = "FT_CLIENT_REFRESH_INTERVAL";
-const DEFAULT_REFRESH_INTERVAL = "10s"; //10 sec
+const DEFAULT_REFRESH_INTERVAL = 10000; //10 sec
 
 export interface ServerArgs {
   ftServerEndPoint: string;
@@ -20,16 +19,17 @@ export function getServerArgs(): ServerArgs {
   endpoint = endpoint.replace(/\/?$/, "/api/");
 
   //get refresh interval
-  let interval = process.env[ENV_REFRESH_INTERVAL_NAME];
+  const intervalEnv = process.env[ENV_REFRESH_INTERVAL_NAME];
+  let interval;
   // do not parse undefined, empty...
-  if (interval) {
-    interval = parseDuration(interval);
+  if (intervalEnv) {
+    interval = parseDuration(intervalEnv);
   }
   // parse parseDuration result
   if (!interval) {
-    interval = parseDuration(DEFAULT_REFRESH_INTERVAL);
-    log(`[INFO] client refresh interval not set or in incorrect pattern, using the default interval: ${DEFAULT_REFRESH_INTERVAL}`);
+    interval = DEFAULT_REFRESH_INTERVAL;
+    log(`[ERROR] client refresh interval not set or in incorrect pattern, using the default interval: ${DEFAULT_REFRESH_INTERVAL}`);
   }
   log(`client refresh interval is: ${interval}`);
-  return { ftServerEndPoint: endpoint, ftServerInterval: Number(interval) };
+  return { ftServerEndPoint: endpoint, ftServerInterval: interval };
 }
