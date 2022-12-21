75087e2b575601af1185036e815f68cadb2d254f
michawai <michal.tall@sap.com>
2022-12-21 12:50:27 +0200

    Update logger.ts

--- src/logger.ts
+++ src/logger.ts
@@ -3,6 +3,6 @@ export const SHOW_LOG_ENV = "SHOW_LOG";
 export function log(logMessage: string): void {
   const showLog = process.env[SHOW_LOG_ENV];
   if (showLog) {
-    console.log('[Feature Toggle]' + logMessage);
+    console.log(`[Feature Toggle] ${logMessage}`);
   }
 }
